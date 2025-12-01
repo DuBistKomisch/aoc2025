a88d41c08a173dcf83f8b0a2c40d8645b92ef130
Jake Barnes <me@jakebarn.es>
2025-12-01 23:17:26 +1100

    Add benchmarking

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { text } from 'node:stream/consumers';
 
+const run = (supplier) => {
+  const start = performance.now();
+  const output = supplier();
+  const end = performance.now();
+  console.log(output);
+  console.log(`${(end - start).toFixed(2)}ms`);
+};
+
 const input = await text(process.stdin);
 const { p1, p2 } = await import('./' + process.argv[2]);
 console.log('Part 1:');
-console.log(p1(input));
+run(() => p1(input));
 console.log('Part 2:');
-console.log(p2(input));
+run(() => p2(input));
