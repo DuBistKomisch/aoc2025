ba6434aec8fc8da456133b858cbb097923431404
Jake Barnes <me@jakebarn.es>
2025-12-12 13:50:39 +1100

    Day 9 Part 2

--- src/d09.ts
+++ src/d09.ts
@@ -1,3 +1,5 @@
+import { Point } from './point';
+
 export function p1(input: string) {
   const points = input.trim().split('\n')
     .map(line => line.split(',').map(n => parseInt(n, 10)));
@@ -18,5 +20,70 @@ export function p1(input: string) {
 };
 
 export function p2(input: string) {
-  return 0;
+  const points = input.trim().split('\n')
+    .map(line => {
+      const [x, y] = line.split(',').map(n => parseInt(n, 10));
+      return new Point(x, y);
+    });
+
+  const xs = [...new Set(points.map(point => point.x).sort((a, b) => a - b))];
+  const ys = [...new Set(points.map(point => point.y).sort((a, b) => a - b))];
+  const minipoints = points.map(point => new Point(xs.indexOf(point.x), ys.indexOf(point.y)));
+
+  const walls = new Set();
+  for (let i = 0; i < minipoints.length; i++) {
+    const a = minipoints[i];
+    const b = minipoints[(i + 1) % minipoints.length];
+    if (a.x === b.x) {
+      for (let j = Math.min(a.y, b.y); j <= Math.max(a.y, b.y); j++) {
+        walls.add(new Point(a.x, j).hash());
+      }
+    } else if (a.y === b.y) {
+      for (let j = Math.min(a.x, b.x); j <= Math.max(a.x, b.x); j++) {
+        walls.add(new Point(j, a.y).hash());
+      }
+    }
+  }
+
+  const outside = new Set();
+  let queue = [new Point(-1, -1)];
+  while (queue.length) {
+    const nextQueue = [];
+    for (const point of queue) {
+      if (point.x < -1 || point.x > xs.length || point.y < -1 || point.y > ys.length) {
+        continue;
+      }
+      const hash = point.hash();
+      if (outside.has(hash) || walls.has(hash)) {
+        continue;
+      }
+      outside.add(hash);
+      nextQueue.push(point.north(), point.east(), point.south(), point.west());
+    }
+    queue = nextQueue;
+  }
+
+  let bestArea = 0;
+  for (let i = 0; i < minipoints.length; i++) {
+    next: for (let j = i + 1; j < minipoints.length; j++) {
+      const a = points[i];
+      const b = points[j];
+      const area = (Math.abs(a.x - b.x) + 1) * (Math.abs(a.y - b.y) + 1);
+      if (area <= bestArea) {
+        continue;
+      }
+      const minia = minipoints[i];
+      const minib = minipoints[j];
+      for (let x = Math.min(minia.x, minib.x); x <= Math.max(minia.x, minib.x); x++) {
+        for (let y = Math.min(minia.y, minib.y); y <= Math.max(minia.y, minib.y); y++) {
+          if (outside.has(new Point(x, y).hash())) {
+            continue next;
+          }
+        }
+      }
+      bestArea = area;
+    }
+  }
+
+  return bestArea;
 };
--- src/util.ts
+++ src/util.ts
@@ -1,1 +1,13 @@
+import { Point } from './point';
+
 export const sum = (acc, n) => acc + n;
+
+export const printGrid = (title: string, set: Set<String>, width: number, height: number) => {
+  console.log(title);
+  for (let j = 0; j < height; j++) {
+    for (let i = 0; i < width; i++) {
+      process.stdout.write(set.has(new Point(i, j).hash()) ? '#' : '.');
+    }
+    process.stdout.write('\n');
+  }
+};
