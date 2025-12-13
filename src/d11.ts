fe2365f5a2993cb714751e5e0f97619521cfca1d
Jake Barnes <me@jakebarn.es>
2025-12-13 15:29:13 +1100

    Day 11 Part 2

--- src/d11.ts
+++ src/d11.ts
@@ -1,4 +1,20 @@
 export function p1(input: string) {
+  const [edges, reverse] = buildEdges(input);
+  return countPaths(edges, reverse, 'you', 'out');
+};
+
+export function p2(input: string) {
+  const [edges, reverse] = buildEdges(input);
+  const svr2fft = countPaths(edges, reverse, 'svr', 'fft', ['dac']);
+  const fft2dac = countPaths(edges, reverse, 'fft', 'dac');
+  const dac2out = countPaths(edges, reverse, 'dac', 'out', ['fft'])
+  const svr2dac = countPaths(edges, reverse, 'svr', 'dac', ['fft'])
+  const dac2fft = countPaths(edges, reverse, 'dac', 'fft')
+  const fft2out = countPaths(edges, reverse, 'fft', 'out', ['dac'])
+  return svr2fft * fft2dac * dac2out + svr2dac * dac2fft * fft2out;
+};
+
+const buildEdges = (input: string) => {
   const edges = new Map();
   const reverse = new Map();
   const lines = input.trim().split('\n');
@@ -11,24 +27,31 @@ export function p1(input: string) {
       reverse.getOrInsert(to, []).push(from);
     }
   }
-  console.log(edges);
-  console.log(reverse);
+  return [edges, reverse];
+};
+
+const countPaths = (
+  edges: Map<string, string[]>,
+  reverse: Map<string, string[]>,
+  from: string, to: string,
+  except: string[] = []
+) => {
   const paths = new Map();
-  paths.set('you', 1);
-  let queue = edges.get('you');
+  paths.set(from, 1);
+  let queue = edges.get(from);
   while (queue.length > 0) {
     const nextQueue = [];
     for (const node of queue) {
-      paths.set(node, reverse.getOrInsert(node, [])
-        .reduce((acc, prev) => acc + paths.getOrInsert(prev, 0), 0))
-      nextQueue.push(...edges.getOrInsert(node, []));
+      const updatedPaths = reverse.getOrInsert(node, [])
+        .reduce((acc, prev) => acc + paths.getOrInsert(prev, 0), 0);
+      if (updatedPaths === paths.get(node)) {
+        continue;
+      }
+      paths.set(node, updatedPaths);
+      nextQueue.push(...edges.getOrInsert(node, [])
+        .filter(edge => !except.includes(edge)));
     }
     queue = nextQueue;
   }
-  console.log(paths);
-  return paths.get('out');
-};
-
-export function p2(input: string) {
-  return 0;
+  return paths.getOrInsert(to, 0);
 };
