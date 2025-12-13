3ff8d752dfe05c8838eee5a23b36e62ef7efdd7d
Jake Barnes <me@jakebarn.es>
2025-12-13 15:28:47 +1100

    Day 10 Part 2

--- src/d10.ts
+++ src/d10.ts
@@ -1,3 +1,5 @@
+import { solve } from 'yalps';
+
 export function p1(input: string) {
   const lines = input.trim().split('\n');
   let answer = 0;
@@ -33,7 +35,37 @@ export function p1(input: string) {
 };
 
 export function p2(input: string) {
-  return 0;
+  const lines = input.trim().split('\n');
+  let answer = 0;
+  for (const line of lines) {
+    const parts = line.split(' ');
+    const buttons = parts.slice(1, -1)
+      .map(button => button.substring(1, button.length - 1).split(',')
+          .map(index => parseInt(index, 10)));
+    const target = parts[parts.length - 1].substring(1, parts[parts.length - 1].length - 1)
+      .split(',').map(n => parseInt(n, 10));
+
+    const input = {
+      direction: 'minimize',
+      objective: 'presses',
+      variables: Object.fromEntries(
+        buttons.map((button, index) => ['b' + index, {
+          presses: 1,
+          ...Object.fromEntries(
+            button.map((index) => [index, 1])
+          )
+        }])
+      ),
+      constraints: Object.fromEntries(
+        target.map((n, index) => [index, { equal: n }])
+      ),
+      integers: buttons.map((_, index) => 'b' + index)
+    };
+    const solution = solve(input);
+
+    answer += solution.result;
+  }
+  return answer;
 };
 
 const indicesToBinary = (indices: number[]) => {
